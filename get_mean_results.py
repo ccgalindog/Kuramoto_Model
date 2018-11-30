3d4a6b7d07e176917761b052116a62b0ac34d53d
Cristian Camilo Galindo González <44300619+ccgalindog@users.noreply.github.com>
2018-11-30 10:38:36 -0500

    Improved, generalized

--- get_mean_results.py
+++ get_mean_results.py
@@ -6,10 +6,12 @@ from mpl_toolkits.mplot3d import Axes3D
 from glob import glob
 import click
 
+# In this file you find the functions:
+# get_result
+# get_mean_results
 
 def get_result(result_file, stead_time, tim_step):
 	my_data = [line.rstrip('\n') for line in open(result_file, "r")]
-
 	x_data = np.loadtxt(my_data)
 	x = x_data[:,1:-2]
 	N = int((x.shape[1])/2)
@@ -19,58 +21,44 @@ def get_result(result_file, stead_time, tim_step):
 	Mag_r = np.sqrt(np.square(Re_r) + np.square(Im_r))
 	phases = ( x[:,0:N] + np.pi) % (2 * np.pi ) - np.pi
 	phase_velocity = x[:,N:-1]
-
-	
 	stead_point = int(stead_time/tim_step)
-
 	phase_velocity_sq = np.square(phase_velocity[stead_point:,:])
-
 	v_inf = np.mean(np.mean(phase_velocity_sq, axis = 1), axis = 0)
-
 	r_inf = np.mean(Mag_r[stead_point:])
 	r_real_inf = np.mean(Re_r[stead_point:])
 	r_imag_inf = np.mean(Im_r[stead_point:])
-
 	return r_inf, r_real_inf, r_imag_inf, v_inf;
 
+####################################################
 
-def main():
-	std_time = 3000
-	t_step = 0.01*10
-	sets_files = glob("Sim_Settings/*.txt")
-
-	for a_set_file in sets_files:
-		
-		one_set_name = a_set_file.replace("Sim_Settings/set_", "")
-		one_set_name = one_set_name.replace(".txt", "")
-
-		res_files = sorted(glob("Results/out_{}*".format(one_set_name)))
-
-		r_all_inf = list()
-		real_all_inf = list()
-		imag_all_inf = list()
-		k_all_inf = list()
-		v_all_inf = list()
-
-		net_file = res_files[0].replace("Results/out_", "")
-		net_file = net_file[0:-16]
-
-
-		mean_out = "Results/mean_results_" + net_file + "_.txt"
+def get_mean_results(std_time, t_step, steps, folders, key_index):
+'''
+INPUT:
+std_time: <Double> - Time at which steady state time is taken.
+t_step: <Double> - time used as the integration step time.
+steps: <Int> - steps taken before printing when the simulation was run.
+folders: <List> - Folders that contain files to average. Each folder generates one mean file.
+key_index: <Int> - Position in the name of the result files after splitting with "_", such that it
+		   contains the element to take as identifier for averaging.
+'''
+	t_step = t_step*steps
+	for folder in folders:
+		sets_files = sorted( glob( "{}/out_*.txt".format(folder) ) )
+		mean_out = "{}/mean_results_{}_.txt".format(folder, folder)
 		whole_results = open(mean_out, "w")
-
-		for re_file in res_files:
-			r_inf_j, real_j, imag_j, v_inf_j = get_result(re_file, std_time, t_step)
+		for a_set_file in sets_files:
+			r_all_inf = list()
+			real_all_inf = list()
+			imag_all_inf = list()
+			k_all_inf = list()
+			v_all_inf = list()
+			r_inf_j, real_j, imag_j, v_inf_j = get_result(a_set_file, std_time, t_step)
 			r_all_inf.append(r_inf_j)
 			real_all_inf.append(real_j)
 			imag_all_inf.append(imag_j)
 			v_all_inf.append(v_inf_j)
-			k_j = float(re_file.split("_")[-2])
+			k_j = float(a_set_file.split("_")[key_index])
 			k_all_inf.append(k_j)
 			whole_results.write("{} {} {} {} {} \n".format(k_j, r_inf_j, real_j, imag_j, v_inf_j))
 
 		whole_results.close()
-
-
-if __name__ == '__main__':
-	main()
