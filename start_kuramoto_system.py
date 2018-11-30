8c8cc15895c11f1fea5ab182282b7ad4d73d93bf
Cristian Camilo Galindo González <44300619+ccgalindog@users.noreply.github.com>
2018-11-30 11:02:24 -0500

    added execute_kuramoto here cuz ynaut

--- start_kuramoto_system.py
+++ start_kuramoto_system.py
@@ -2,6 +2,35 @@ import numpy as np
 import os
 from sklearn.utils import shuffle
 
+
+def get_to_run_file(boost_dir):
+'''
+INPUT:
+boost_dir: <String> - Location of Boost directory.
+OUTPUT:
+to_run.sh file which lists all the simulations that need to be run.
+'''
+	all_sets_files = glob("Sim_Settings/*.txt")
+	flags_compiler = "c++ -O3 -Wall -ffast-math -std=c++11"
+	functions_name = "kuramoto_functions.cpp"
+	libout_name = "kuramoto_functions.o"
+	file_name = "kuramoto_functions.cpp"
+	main_name = "main_kuramoto.cpp"
+	file_base_name = "main_kuramoto_grid"
+	simulation_commands = list()
+	os.system("{} -I {} -c {}".format(flags_compiler, boost_dir, functions_name))
+	k = 0
+	for sim_set_file in all_sets_files:
+		file_base_name_new = file_base_name + "_{}".format(k)
+		simulation_commands.append("{} -I {} {} {} -o {} && time ./{}>Results/Testing_out.txt {}".format(flags_compiler, boost_dir, libout_name, main_name, file_base_name_new, file_base_name_new, sim_set_file))
+		k = k+1
+	sims_2_run = open("to_run.sh", "w")
+	for a_sim in simulation_commands:
+		sims_2_run.write(a_sim + "\n")
+	sims_2_run.close()
+
+######################################################################################################
+	
 def default_constructor(type_net, dyn_model, delt_d, consumers, Pc, Psg, Pbg, damp):
 
 	if (type_net == "2n"):
